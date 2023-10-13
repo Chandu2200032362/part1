75bd8cb0c986dabe39e952b78cad8d4975ce00e4
Chandu2200032362 <137592282+Chandu2200032362@users.noreply.github.com>
2023-10-13 15:11:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import surveyform from './surveyform';
+
+function App() {
+  return (
+    <div>
+      <h1> My React App</h1>
+      <surveyform/>
+    </div>
+  );
+}
+
+export default App;
